a85b26e62c5cb37903e9d7d823b86029b12d4d51
Olesia <melnuchun@icloud.com>
2021-07-19 21:25:54 +0200

    logo due to the theme of the browser

--- src-web/Components/Header.js
+++ src-web/Components/Header.js
@@ -5,18 +5,25 @@ import './Header.css';
 import { Button } from '@material-ui/core';
 
 
-const logout= (userHasAuthenticated) =>{
+const logout = (userHasAuthenticated) => {
     Cookies.remove("user");
     userHasAuthenticated(false)
 }
 
 const Header = (props) => {
+    const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+    if (!darkThemeMq.matches) {
+
+        document.getElementById("favi").href = "./assets/logo_disconnect.ico"
+    } else {
+        document.getElementById("favi").href = "./assets/logo_disconnect-black.ico"
+    }
     return (
         <div className="header">
 
-            <img src={logo} alt='logo_disconnect'/>
-            { props.isAuthenticated && 
-            <a onClick={()=>{logout(props.userHasAuthenticated)}}>LOG OUT</a>
+            <img src={logo} alt='logo_disconnect' />
+            {props.isAuthenticated &&
+                <a onClick={() => { logout(props.userHasAuthenticated) }}>LOG OUT</a>
             }
         </div>
     )
